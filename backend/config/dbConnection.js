4eae50ad00a4ff31bc53eedc9ae345e9977f99e5
unknown <harkrish2504.com git config --global user.name Harikrishnan>
2024-08-10 11:09:12 +0530

    FINAL COMMIT...

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,11 +1,13 @@
-const mongoose = require("mongoose")
-const connectDB = async ()=>{
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-        console.log("Database connected",connect.connection.host,connect.connection.name);
+const mongoose = require("mongoose");
+const CONNECTION_STRING =
+  "mongodb+srv://harkrish2504:Q3yyu26h48@cluster0.6emamdo.mongodb.net/ReactBlog?retryWrites=true&w=majority&appName=Cluster0";
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-module.exports=connectDB;
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(CONNECTION_STRING);
+    console.log("Database connected", connect.connection.host, connect.connection.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = connectDB;
